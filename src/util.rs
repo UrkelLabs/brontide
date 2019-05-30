ee8225afe88774b3eb90ba8c6673103b60873954
kilpatty <seanpkilgarriff@gmail.com>
2019-05-30 17:12:24 -0500

    move expand to util

---
+++ src/util.rs
@@ -0,0 +1,13 @@
+use hkdf::Hkdf;
+use sha2::{Digest, Sha256};
+
+fn expand(secret: [u8; 32], salt: [u8; 32]) -> (Vec<u8>, Vec<u8>) {
+    //hk.prk
+    let hk = Hkdf::<Sha256>::extract(Some(&salt), &secret);
+    let mut out = [0u8; 64];
+    //TODO remove unwrap
+    hk.expand(&[], &mut out).unwrap();
+
+    //TODO double check this
+    (out[0..32].to_vec(), out[32..64].to_vec())
+}
