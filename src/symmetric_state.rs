b8c5ba918f820c6d038a7fbe58b6aa03403e4069
kilpatty <seanpkilgarriff@gmail.com>
2019-06-04 13:57:58 -0500

    symmetric: fix compilation errors -> needs more work, but compiles

--- src/symmetric_state.rs
+++ src/symmetric_state.rs
@@ -43,15 +43,20 @@ impl SymmetricState {
     }
 
     //TODO test if tag as an option handles this behavior correctly.
-    pub fn mix_hash(&mut self, data: Buffer, tag: Option<Buffer>) {
-        self.digest = self.mix_digest(data, tag);
-    }
+    // pub fn mix_hash(&mut self, data: &[u8], tag: Option<&[u8]>) {
+    //     let digest = self.mix_digest(data, tag);
+
+    //     self.digest.copy_from_slice(&digest);
+    // }
 
     //TODO review
-    pub fn mix_digest(&mut self, data: Buffer, tag: Option<Buffer>) -> Buffer {
+    //Not sure we need the tag here
+    //this applies to the above function as well, but we can essentially just add the tag
+    //to the end of the data, and it should work fine.
+    pub fn mix_digest(&mut self, data: &[u8], tag: Option<&[u8]>) {
         let mut hasher = Sha256::new();
 
-        hasher.input(self.digest);
+        hasher.input(&self.digest);
         hasher.input(data);
         if let Some(tag_ok) = tag {
             hasher.input(tag_ok);
@@ -59,26 +64,36 @@ impl SymmetricState {
 
         let result = hasher.result();
 
-        Buffer::from(result.as_slice().to_vec())
+        self.digest.copy_from_slice(&result);
     }
 
     //pt = plaintext, let's make that more verbose TODO so the code is more readable.
-    pub fn encrypt_hash(&mut self, pt: Buffer) -> Buffer {
-        let tag = self.cipher.encrypt(pt, self.digest);
+    //
+    ////TODO should return custom tag type.
+    pub fn encrypt_hash(&mut self, plain_text: &[u8]) -> [u8; 16] {
+        //TODO remove this, and have this function return a result
+        let tag = self.cipher.encrypt(plain_text, &self.digest).unwrap();
 
-        self.mix_hash(pt, Some(tag));
+        self.mix_digest(plain_text, Some(&tag));
 
-        tag
+        let mut return_tag = [0_u8; 16];
+
+        return_tag.copy_from_slice(&tag);
+
+        return_tag
     }
 
     //ct == CipherText, make this more verbose as above TODO
-    pub fn decrypt_hash(&mut self, ct: Buffer, tag: Buffer) -> bool {
-        let digest = self.mix_digest(ct, Some(tag));
-
-        let result = self.cipher.decrypt(ct, tag, self.digest);
+    //TODO, make this it's own type - tag - as MAC.
+    pub fn decrypt_hash(&mut self, cipher_text: &[u8], tag: [u8; 16]) -> bool {
+        let result = self.cipher.decrypt(cipher_text, &tag, &self.digest);
 
         if result {
-            self.digest = digest;
+            //TODO this is different than in HSD, test that this works (Calling mix_digest *after*
+            //the decrypt call. If it does, open a PR for this.
+            self.mix_digest(cipher_text, Some(&tag));
+            //TODO double check this.
+            // self.digest.copy_from_slice(&digest);
             true
         } else {
             false
