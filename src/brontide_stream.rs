8f2210196ac59ad3aebeafdee1984d20ce90596f
kilpatty <seanpkilgarriff@gmail.com>
2019-06-06 17:20:00 -0500

    brontide-stream: fix compilation errors on futures, change state enum

--- src/brontide_stream.rs
+++ src/brontide_stream.rs
@@ -1,31 +1,36 @@
 use crate::brontide::Brontide;
-// use futures::io::{AsyncRead, AsyncWrite};
-use futures::prelude::*;
+// use crate::Result;
+use futures::io::Error;
+use futures::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
+use std::marker::Unpin;
+// #![feature(async_await)]
+// use futures::prelude::*;
 
 //TODO not sure if we want this to be all caps syntax, or if we follow other enum syntax.
 //Look into common practices here around "constant" enums
-enum State {
-    ACT_NONE,
-    ACT_ONE,
-    ACT_TWO,
-    ACT_THREE,
+//TODO fix clippy issue of all prefixes being act
+enum ActState {
+    None,
+    One,
+    Two,
+    Three,
 }
 
 //TODO this might actually have to be AsyncReadExt and AsyncWriteExt to have the functions I'm
 //using.
 pub struct BrontideStream<T>
 where
-    T: AsyncReadExt + AsyncWriteExt,
+    T: AsyncRead + AsyncWrite + AsyncReadExt + AsyncWriteExt + Unpin,
 {
     socket: T,
-    state: State,
+    state: ActState,
     brontide: Brontide,
 }
 
 //TODO check on this.... Not sure if we are doing the impl correctly
-impl BrontideStream<T>
+impl<T> BrontideStream<T>
 where
-    T: AsyncReadExt + AsyncReadExt,
+    T: AsyncRead + AsyncWrite + AsyncReadExt + AsyncReadExt + Unpin,
 {
     //TODO just make this config
     // fn read_timeout() -> Duration {
@@ -45,31 +50,34 @@ where
         //TODO check naming here
         self.brontide = Brontide::new(true, local_secret, Some(remote_public));
 
-        //Probably want to await this here.
-        self.start();
+        //Probably want to await this here. TODO
+        self.start(socket);
     }
 
     //TODO we probably want this returning a future.
-    pub fn start(&mut self, socket: T) {
-        if self.initiator {
-            self.state = State::ActTwo;
+    pub async fn start(&mut self, mut socket: T) -> Result<(), Error> {
+        //TODO instead of doing this, I should expose this as a function
+        if self.brontide.initiator() {
+            self.state = ActState::Two;
             //TODO not sure if we need this I think we can infer size from the socket.
             // self.waiting = Act_two_size;
             let act_one = self.brontide.gen_act_one();
             //TODO await this.
-            socket.write_all(act_one); //.await?
-                                       //Catch the error either here, or above and destroy -> I"m thinking above.
+            socket.write_all(&act_one).await?;
+        //Catch the error either here, or above and destroy -> I"m thinking above.
         } else {
-            self.state = State::ACT_ONE;
+            self.state = ActState::One;
             //TODO as above check if this is needed, I think we can infer from reading the stream.
             // self.waiting = act_one_size
         }
+
+        Ok(())
     }
 }
 
 impl<T> AsRef<T> for BrontideStream<T>
 where
-    T: AsyncReadExt + AsyncWriteExt,
+    T: AsyncReadExt + AsyncWriteExt + Unpin,
 {
     fn as_ref(&self) -> &T {
         &self.socket
@@ -78,7 +86,7 @@ where
 
 impl<T> AsMut<T> for BrontideStream<T>
 where
-    T: AsyncReadExt + AsyncWriteExt,
+    T: AsyncReadExt + AsyncWriteExt + Unpin,
 {
     fn as_mut(&mut self) -> &mut T {
         &mut self.socket
