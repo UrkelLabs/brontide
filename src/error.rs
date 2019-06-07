de1f0b58ca33fe6d692df23b6e21fdd3a165143c
kilpatty <seanpkilgarriff@gmail.com>
2019-06-07 11:16:54 -0500

    error: add more error types - mainly for acts

--- src/error.rs
+++ src/error.rs
@@ -1,22 +1,37 @@
-use chacha20_poly1305_aead::DecryptError;
+use futures;
+use std;
 use std::fmt;
-use std::io;
 
 #[derive(Debug)]
 pub enum Error {
-    Io(io::Error),
+    FutureIo(futures::io::Error),
+    Io(std::io::Error),
+    //TODO naming on these. - technically all "bad-xyz"
+    Version(String),
+    BadKey(String),
+    BadTag(String),
 }
 
-impl From<io::Error> for Error {
-    fn from(e: io::Error) -> Error {
+impl From<std::io::Error> for Error {
+    fn from(e: std::io::Error) -> Self {
         Error::Io(e)
     }
 }
+
+// impl From<futures::io::Error> for Error {
+//     fn from(e: futures::io::Error) -> Self {
+//         Error::FutureIo(e)
+//     }
+// }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             Error::Io(ref e) => write!(f, "IO error: {}", e),
+            Error::FutureIo(ref e) => write!(f, "IO Future error: {}", e),
+            Error::Version(ref e) => write!(f, "Version error: {}", e),
+            Error::BadKey(ref e) => write!(f, "Bad Key error: {}", e),
+            Error::BadTag(ref e) => write!(f, "Bad Tag error: {}", e),
         }
     }
 }
