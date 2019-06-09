bda46cd72fa20f469b96ada0eb9816cab328114f
kilpatty <seanpkilgarriff@gmail.com>
2019-06-09 17:44:33 -0500

    tests: add read into the tests

--- tests/bolt_0008_test.rs
+++ tests/bolt_0008_test.rs
@@ -127,9 +127,42 @@ fn initiator_setup() -> brontide::Brontide {
     initiator
 }
 
+fn responder_setup() -> brontide::Brontide {
+    let mut ls_priv = [0_u8; 32];
+    ls_priv.copy_from_slice(
+        &hex::decode("2121212121212121212121212121212121212121212121212121212121212121").unwrap(),
+    );
+
+    let mut responder = brontide::Brontide::new(false, ls_priv, None, Some(PROLOGUE));
+
+    responder.handshake_state.generate_key = || {
+        let mut e_priv = [0_u8; 32];
+        e_priv.copy_from_slice(
+            &hex::decode("2222222222222222222222222222222222222222222222222222222222222222")
+                .unwrap(),
+        );
+        e_priv
+    };
+
+    let mut act_one = [0_u8; 50];
+    act_one.copy_from_slice(&hex::decode("00036360e856310ce5d294e8be33fc807077dc56ac80d95d9cd4ddbd21325eff73f70df6086551151f58b8afe6c195782c6a").unwrap());
+
+    let result = responder.recv_act_one(act_one);
+
+    let act_two = responder.gen_act_two();
+
+    let mut act_three = [0_u8; 66];
+    act_three.copy_from_slice(&hex::decode("00b9e3a702e93e3a9948c2ed6e5fd7590a6e1c3a0344cfc9d5b57357049aa22355361aa02e55a8fc28fef5bd6d71ad0c38228dc68b1c466263b47fdf31e560e139ba").unwrap());
+
+    let result = responder.recv_act_three(act_three);
+
+    responder
+}
+
 #[test]
 fn test_encryption_and_key_rotation() {
     let mut initiator = initiator_setup();
+    let mut responder = responder_setup();
 
     let HELLO = b"hello";
 
@@ -138,36 +171,34 @@ fn test_encryption_and_key_rotation() {
 
         match x {
             0 => assert_eq!(
-                hex::encode(packet),
+                hex::encode(&packet),
                 "cf2b30ddf0cf3f80e7c35a6e6730b59fe802473180f396d88a8fb0db8cbcf25d2f214cf9ea1d95"
             ),
             1 => assert_eq!(
-                hex::encode(packet),
+                hex::encode(&packet),
                 "72887022101f0b6753e0c7de21657d35a4cb2a1f5cde2650528bbc8f837d0f0d7ad833b1a256a1"
             ),
             500 => assert_eq!(
-                hex::encode(packet),
+                hex::encode(&packet),
                 "178cb9d7387190fa34db9c2d50027d21793c9bc2d40b1e14dcf30ebeeeb220f48364f7a4c68bf8"
             ),
             501 => assert_eq!(
-                hex::encode(packet),
+                hex::encode(&packet),
                 "1b186c57d44eb6de4c057c49940d79bb838a145cb528d6e8fd26dbe50a60ca2c104b56b60e45bd"
             ),
             1000 => assert_eq!(
-                hex::encode(packet),
+                hex::encode(&packet),
                 "4a2f3cc3b5e78ddb83dcb426d9863d9d9a723b0337c89dd0b005d89f8d3c05c52b76b29b740f09"
             ),
             1001 => assert_eq!(
-                hex::encode(packet),
+                hex::encode(&packet),
                 "2ecd8c8a5629d0d02ab457a0fdd0f7b90a192cd46be5ecb6ca570bfc5e268338b1a16cf4ef2d36"
             ),
             _ => {}
         }
+
+        let message = responder.read(&packet);
+
+        assert_eq!(hex::encode(message), hex::encode(HELLO));
     }
 }
-
-//       const msg = responder.read(packet);
-
-//       assert.strictEqual(msg.toString('hex'), HELLO.toString('hex'));
-//     }
-//   });
