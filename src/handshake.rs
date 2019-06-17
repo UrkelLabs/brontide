51df9be6cfe2cf898be90503d1173999ad66d9bf
kilpatty <seanpkilgarriff@gmail.com>
2019-06-17 08:48:32 -0500

    handshake: convert to custom types, clean up unused functions

--- src/handshake.rs
+++ src/handshake.rs
@@ -1,73 +1,52 @@
 use crate::common::PROTOCOL_NAME;
 use crate::symmetric_state::SymmetricState;
+use crate::types::{PublicKey, SecretKey};
+use crate::Result;
 use hex;
 use secp256k1::rand::rngs::OsRng;
-use secp256k1::{PublicKey, Secp256k1, SecretKey};
+use secp256k1::{self, Secp256k1};
+use std::str::FromStr;
 
-//TODO review pub exposure
 pub struct HandshakeState {
     pub(crate) symmetric: SymmetricState,
     pub(crate) initiator: bool,
-    //TODO again all these should be custom
-    pub(crate) local_static: [u8; 32],
-    pub(crate) local_ephemeral: [u8; 32],
-    //TODO both these need to be 33.
-    pub(crate) remote_static: [u8; 33],
-    pub(crate) remote_ephemeral: [u8; 33],
-    pub generate_key: fn() -> [u8; 32],
+    pub(crate) local_static: SecretKey,
+    pub(crate) local_ephemeral: SecretKey,
+    pub(crate) remote_static: PublicKey,
+    pub(crate) remote_ephemeral: PublicKey,
+    pub generate_key: fn() -> Result<SecretKey>,
 }
 
 impl HandshakeState {
-    //pub fn generate_key() -> [u8; 32] {
-    //    let secp = Secp256k1::new();
-    //    let mut rng = OsRng::new().expect("OsRng");
-    //    let (secret_key, _) = secp.generate_keypair(&mut rng);
-
-    //    //TODO redo this.
-    //    let mut key = [0_u8; 32];
-    //    //TODO capture this unwrap and handle accordling
-    //    key.copy_from_slice(&hex::decode(secret_key.to_string()).unwrap());
-
-    //    key
-    //}
-
     pub(crate) fn new(
         initiator: bool,
         prologue: &str,
-        local_pub: [u8; 32],
-        remote_pub: Option<[u8; 33]>,
+        local_pub: SecretKey,
+        remote_pub: Option<PublicKey>,
     ) -> Self {
-        let remote_public_key: [u8; 33];
+        let remote_public_key: PublicKey;
 
         if let Some(remote_pub_ok) = remote_pub {
             remote_public_key = remote_pub_ok
         } else {
-            //Should be zero key not buffer new, TODO
-            remote_public_key = [0_u8; 33];
+            remote_public_key = PublicKey::empty();
         }
-
-        //Need constants here TODO
-        //ZERO_PUB
-        //ZERO_KEY
 
         let mut state = HandshakeState {
             initiator,
             local_static: local_pub,
             remote_static: remote_public_key,
             symmetric: SymmetricState::new(PROTOCOL_NAME),
-            local_ephemeral: [0; 32],
-            remote_ephemeral: [0; 33],
+            local_ephemeral: SecretKey::empty(),
+            remote_ephemeral: PublicKey::empty(),
             generate_key: || {
                 let secp = Secp256k1::new();
                 let mut rng = OsRng::new().expect("OsRng");
                 let (secret_key, _) = secp.generate_keypair(&mut rng);
 
-                //TODO redo this.
-                let mut key = [0_u8; 32];
-                //TODO capture this unwrap and handle accordling
-                key.copy_from_slice(&hex::decode(secret_key.to_string()).unwrap());
+                let key = SecretKey::from_str(secret_key.to_string().as_ref())?;
 
-                key
+                Ok(key)
             },
         };
 
@@ -79,9 +58,9 @@ impl HandshakeState {
         } else {
             let secp = Secp256k1::new();
 
-            let secret_key =
-                SecretKey::from_slice(&state.local_static).expect("32 bytes, within curve order");
-            let public_key = PublicKey::from_secret_key(&secp, &secret_key);
+            let secret_key = secp256k1::SecretKey::from_slice(&state.local_static)
+                .expect("32 bytes, within curve order");
+            let public_key = secp256k1::PublicKey::from_secret_key(&secp, &secret_key);
 
             state
                 .symmetric
@@ -89,49 +68,5 @@ impl HandshakeState {
         }
 
         state
-    }
-
-    //TODO remove this.
-    pub fn init_state(
-        &mut self,
-        initiator: bool,
-        prologue: &str,
-        local_pub: [u8; 32],
-        remote_pub: Option<[u8; 33]>,
-    ) {
-        let remote_public_key: [u8; 33];
-        self.initiator = initiator;
-        //TODO might not have to do this.
-        self.local_static.copy_from_slice(&local_pub);
-        if let Some(remote_pub_ok) = remote_pub {
-            remote_public_key = remote_pub_ok
-        } else {
-            //Should be zero key not buffer new, TODO
-            remote_public_key = [0_u8; 33];
-        }
-
-        self.remote_static = remote_public_key;
-
-        self.symmetric = SymmetricState::new(PROTOCOL_NAME);
-
-        //Might have to make sure this works as ascii TODO
-        self.symmetric.mix_digest(prologue.as_bytes(), None);
-
-        if initiator {
-            //TODO we need to test this behavior, but I think the general idea is we want to mix
-            //this with a zero hash buffer. so 32 bytes of 0s.
-            self.symmetric.mix_digest(&remote_public_key, None)
-        } else {
-            //Switch this with the get public function TODO
-            let secp = Secp256k1::new();
-            //TODO handle this error correctly.
-            let secret_key =
-                SecretKey::from_slice(&local_pub).expect("32 bytes, within curve order");
-            let public_key = PublicKey::from_secret_key(&secp, &secret_key);
-            //TODO review this, not sure I trust converting the public key to string then reading
-            //it in the buffer.
-            self.symmetric
-                .mix_digest(public_key.to_string().as_bytes(), None);
-        }
     }
 }
