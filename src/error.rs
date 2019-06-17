9d048245b6370e2d3c97c7f2088e76944baaa1ad
kilpatty <seanpkilgarriff@gmail.com>
2019-06-17 16:05:01 -0500

    error: add secp256k1 and data too large errors

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,5 @@
 use hex::FromHexError;
+use secp256k1;
 use std;
 use std::fmt;
 
@@ -6,10 +7,12 @@ use std::fmt;
 pub enum Error {
     Io(std::io::Error),
     Hex(FromHexError),
+    Secp256k1(secp256k1::Error),
     //TODO naming on these. - technically all "bad-xyz"
     Version(String),
     BadKey(String),
     BadTag(String),
+    DataTooLarge(String),
 }
 
 impl From<std::io::Error> for Error {
@@ -24,14 +27,22 @@ impl From<FromHexError> for Error {
     }
 }
 
+impl From<secp256k1::Error> for Error {
+    fn from(e: secp256k1::Error) -> Self {
+        Error::Secp256k1(e)
+    }
+}
+
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             Error::Io(ref e) => write!(f, "IO error: {}", e),
             Error::Hex(ref e) => write!(f, "Hex error: {}", e),
+            Error::Secp256k1(ref e) => write!(f, "Secp256k1 error: {}", e),
             Error::Version(ref e) => write!(f, "Version error: {}", e),
             Error::BadKey(ref e) => write!(f, "Bad Key error: {}", e),
             Error::BadTag(ref e) => write!(f, "Bad Tag error: {}", e),
+            Error::DataTooLarge(ref e) => write!(f, "Data too large error: {}", e),
         }
     }
 }
